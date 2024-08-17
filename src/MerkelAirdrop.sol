ae5e7f5c9a893d65de98d58a68810ae4f1ea9e6a
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-17 19:10:32 +0530

    Update: add immutable variable and claimers array

--- src/MerkelAirdrop.sol
+++ src/MerkelAirdrop.sol
@@ -2,6 +2,20 @@
 
 pragma solidity ^0.8.13;
 
+import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 contract MerkelAirdrop{
+
+    address[] public claimers;
+
+    bytes32 private immutable i_merkelProof;
+    IERC20 private immutable i_airDropToken;
+
+    constructor(bytes32 merkelProof, IERC20 airDropToken){
+        i_merkelProof = merkelProof;
+        i_airDropToken = airDropToken;
+    }
+
+    function claim(bytes32 merkelProof, uint256 amount, bytes32[] calldata ) external{}
 
 }
