39859e9ab43b669075a86741b6c2cbc4d32072cb
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-19 09:37:53 +0530

    Update Token & Merkle contract

--- src/MerkelAirdrop.sol
+++ src/MerkelAirdrop.sol
@@ -2,20 +2,34 @@
 
 pragma solidity ^0.8.13;
 
-import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import {IERC20, SafeERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {MerkleProof} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MerkleProof.sol";
 
-contract MerkelAirdrop{
+contract MerkleAirdrop{
+
+    using SafeERC20 for IERC20;
+
+    error MerkleAirdrop__InvalidProof();
 
     address[] public claimers;
 
-    bytes32 private immutable i_merkelProof;
+    bytes32 private immutable i_merkleProof;
     IERC20 private immutable i_airDropToken;
 
-    constructor(bytes32 merkelProof, IERC20 airDropToken){
-        i_merkelProof = merkelProof;
+    event claimed(address account, uint256 amount);
+
+    constructor(bytes32 merkleProof, IERC20 airDropToken){
+        i_merkleProof = merkleProof;
         i_airDropToken = airDropToken;
     }
 
-    function claim(bytes32 merkelProof, uint256 amount, bytes32[] calldata ) external{}
+    function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if(MerkleProof.verify(merkleProof, i_merkleProof, leaf)){
+            revert MerkleAirdrop__InvalidProof();
+        }
+        emit claimed(account, amount);
+        i_airDropToken.transfer(account, amount);
+    }
 
 }
--- src/WagmiToken.sol
+++ src/WagmiToken.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.13;
 import {ERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 
+
 contract WagmiToken is ERC20, Ownable {
     constructor() ERC20("Wagmi","WT") Ownable(msg.sender){
     }
