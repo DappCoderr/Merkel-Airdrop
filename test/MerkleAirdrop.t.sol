0704e610389d31c46ab1ffbeeb10f8448c0f0545
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2024-08-20 20:55:41 +0530

    Update MerkleContract and test file

--- test/MerkleAirdrop.t.sol
+++ test/MerkleAirdrop.t.sol
@@ -2,8 +2,26 @@
 
 pragma solidity ^0.8.13;
 
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {MerkleAirdrop} from "src/MerkleAirdrop.sol";
+import {WagmiToken} from "src/WagmiToken.sol";
 
-contract MerkleAirdrop is Test{
+contract MerkleAirdropTest is Test{
     
+    MerkleAirdrop public airdrop;
+    WagmiToken public token;
+
+    bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
+    address user;
+    uint256 userPrivateKey;
+
+    function setup() public {
+        airdrop = new MerkleAirdrop(ROOT ,token);
+        token = new WagmiToken();
+        (user, userPrivateKey) = makeAddrAndKey("user");
+    }
+
+    function testUserCanClaim() public view {
+        console.log("user address", user);
+    }
 }
